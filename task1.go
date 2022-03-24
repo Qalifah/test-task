dac241e6452f9be4991a6487b32db7b034d382c9
qalifah <oqalifah@gmail.com>
2022-03-24 08:42:55 +0100

    declare testValidity function

---
+++ task1.go
@@ -0,0 +1,7 @@
+package test_task
+
+// testValidity checks if a string is a sequence of numbers followed by dash followed by text
+// function is easy to implement, should take me at most three minutes to implement.
+func testValidity(input string) bool {
+	return false
+}
