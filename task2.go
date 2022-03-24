15a45b87d1ee40e7f58b8a50e7fe89e18df9284f
qalifah <oqalifah@gmail.com>
2022-03-24 10:39:37 +0100

    used time to implement generate function

--- task2.go
+++ task2.go
@@ -9,6 +9,7 @@ import (
 
 // generate takes a boolean flag and generates random correct strings if the parameter is true and random incorrect strings if the flag is false.
 // function is fairly hard to implement, should take me at most 20 minutes to implement.
+// took me around 20 minutes to implement.
 func generate(correct bool) string {
 	if correct {
 		rand.Seed(time.Now().Unix())
