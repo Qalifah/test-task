57ba8f1a841d5d4c90c59f2a63d3db1a9d0db416
qalifah <oqalifah@gmail.com>
2022-03-24 10:18:28 +0100

    declare generate function

---
+++ task2.go
@@ -0,0 +1,7 @@
+package test_task
+
+// generate takes a boolean flag and generates random correct strings if the parameter is true and random incorrect strings if the flag is false.
+// function is fairly hard to implement, should take me at most 20 minutes to implement.
+func generate(correct bool) string {
+	return ""
+}
