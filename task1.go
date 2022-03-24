1aaa9fccd2b39ef1844d9ff26ab0e9ef436ebda0
qalifah <oqalifah@gmail.com>
2022-03-24 08:47:40 +0100

    used time to implement testValidity function

--- task1.go
+++ task1.go
@@ -7,6 +7,7 @@ import (
 
 // testValidity checks if a string is a sequence of numbers followed by dash followed by text
 // function is easy to implement, should take me at most three minutes to implement.
+// took me around 2 minutes to implement.
 func testValidity(input string) bool {
 	pattern := `([0-9]+-[a-zA-Z]+-?)+`
 	matched, err := regexp.MatchString(pattern, input)
